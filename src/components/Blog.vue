9c8d1bed22503708b500403771e756570b239066
Taylor Ren <taylor.ren@gmail.com>
2017-11-23 22:06:24 +0800

    Finished Blog section

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -9,19 +9,19 @@
           <div class="row">
             <div class="showroom-item blog-item col-sm-4" v-for="blog in blogs" :key="blog.post_title">
               <div class="image">
-                <a href ="https://rsywx.net/wordpress/2017/10/31/kazuo-question-and-how-to-better-understand-buried-giants">
-                <img src="https://rsywx.net/wordpress/wp-content/uploads/2017/11/Roberteddison_thumb.jpg"  class="img-responsive" alt="来自KI的拷问以及如何更好地评价《被掩埋的巨人》" title="来自KI的拷问以及如何更好地评价《被掩埋的巨人》">
+                <a :href ="blog.post_title">
+                <img :src="blog.img"  class="img-responsive" :alt="blog.post_title" :title="blog.post_title">
                 </a>
               </div>
               <div class="content">
                 <h3>
-                  <a href ="https://rsywx.net/wordpress/2017/10/31/kazuo-question-and-how-to-better-understand-buried-giants">
-                      来自KI的拷问以及如何更好地评价《被掩埋的巨人》</a>
+                  <a :href ="blog.uri">
+                      {{blog.post_title}}</a>
                 </h3>
-                <span class="meta">发表日期：2017年10月31日</span>
-                <span class="preview">这本《被掩埋的巨人》是我买了KPW3之后，在KPW3上完成的第一本书，所以特别要备注一下。这本书在豆瓣评分将将7.9，还没有到8分……确实有点偏低了。我看了一下，写下评论的读者都给出4-5星的评价，那么给出3星及以下的读者没有给出评论，所以...
+                <span class="meta">发表日期：{{blog.post_date}}</span>
+                <span class="preview">{{blog.text}}...
                 </span>
-                <a class="more" href="https://rsywx.net/wordpress/2017/10/31/kazuo-question-and-how-to-better-understand-buried-giants">浏览全文&nbsp;&nbsp;<span class="red">&raquo;</span></a>
+                <a class="more" :href="blog.uri">浏览全文&nbsp;&nbsp;<span class="red">&raquo;</span></a>
               </div>
             </div>
           </div>
@@ -49,9 +49,33 @@ export default {
         })
         .then(json => {
           var blogs=json.out;
-          this.blogs=blogs.slice(1,4);
+          blogs=blogs.slice(1,4);
+          console.log(blogs);
+          for(var i in blogs) {
+            var blog=blogs[i];
+            var pd=new Date(blog.post_date);
+            var pdy=pd.getFullYear();
+            var pdm=String(pd.getMonth()+1).padStart(2,'0');
+            var pdd=String(pd.getDate()).padStart(2,'0');
+            blog.uri="https://rsywx.net/wordpress/" + pdy + "/" + pdm + "/" + pdd + "/" + blog.post_name;
+            blog.img=this.getImg(blog);
+            blog.text=this.getText(blog);
+          }
+          this.blogs=blogs;
         })
-
+    },
+    getImg(blog) {
+      var html=blog.post_content;
+      var pattern=/<img.+src=['"]([^'"]+)['"].*>/i;
+      var res=html.match(pattern);
+      var img=res[1];
+      return img;
+    },
+    getText(blog) {
+      var div=document.createElement("div");
+      div.innerHTML=blog.post_content;
+      var text=div.textContent||div.innerText||"";
+      return text.substring(0,100);
     }
   },
   created: function() {
