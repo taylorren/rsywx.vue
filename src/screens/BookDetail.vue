b7db7c8cfa6c89641652c37bca4d1d3e944a28b6
Taylor Ren <taylor.ren@gmail.com>
2017-11-24 08:18:14 +0800

    So far so good

--- src/screens/BookDetail.vue
+++ src/screens/BookDetail.vue
@@ -1,7 +1,169 @@
 <template>
   <div>
     <TopNav :active='2'/>
-    <p>This is Book Detail page</p>
+    <div class="widewrapper">
+        <div class="container content">
+            <div class="row">
+                <div class="col-sm-6">
+                    <img :src="book.cover" :alt="book.title+' '+book.author" :title="book.title+' '+book.author"/>
+                </div>
+
+                <div class="col-sm-6">
+                    <h1>{{book.title}}</h1>
+                    <p><strong>ISBN：</strong>{{book.isbn}}<br/>
+                        <strong>作者：</strong>{{book.author}}（{{book.region}}）| 译者：陈实<br/>
+                        <strong>价格：</strong>RMB {{book.price}}<br/>
+                        <strong>购于：</strong>{{book.purchdate}}，{{book.plname}}<br/>
+                    </p>
+                    <div class="feature">
+                        <div class="icon">
+                            <i class="glyphicons tags"></i>
+                        </div>
+                        <div class="text">
+                            <h3>TAG</h3>
+                            <small><a href="/books/list/tag/%E7%BB%8F%E5%85%B8">经典</a>
+<a href="/books/list/tag/%E6%96%87%E5%AD%A6">文学</a>
+<a href="/books/list/tag/%E6%95%A3%E6%96%87">散文</a>
+<a href="/books/list/tag/%E6%84%8F%E5%A4%A7%E5%88%A9">意大利</a>
+</small>
+                            <a class="btn btn-info btn-sm" data-toggle="modal" href="#addtag" >增加更多TAG »</a><br/>
+                        </div>
+                    </div>
+                    <div class="feature">
+                        <div class="icon">
+                            <i class="glyphicons tags"></i>
+                        </div>
+                        <div class="text">
+                            <h3>豆瓣TAG</h3>
+                            <small>卡尔维诺
+隐形的城市
+意大利文学
+意大利
+小说
+外国文学
+看不见的城市
+经典
+</small>
+                        </div>
+                    </div>
+                    <div class="feature">
+                        <div class="icon">
+                            <i class="glyphicons star"></i>
+                        </div>
+                        <div class="text">
+                            <h3>豆瓣评分</h3>
+                            <small>9.0</small>
+                        </div>
+                    </div>
+                    <div class="feature">
+                        <div class="icon">
+                            <i class="glyphicons screenshot"></i>
+                        </div>
+                        <div class="text">
+                            <h3>浏览次数</h3>
+                            <small><strong>2387</strong>（上次访问时间是2017-11-19 14:10:50）</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <h3>简介：</h3>
+                    <p>这是我买的第一本卡尔维诺的书。此后就一发不可收拾。
+
+曾经重复读过N多次，还做了若干主页向大家推荐（早在98、99年）。现在吗，看卡尔维诺已经很时髦了。
+
+别忘了，这本书初版只有1870册的印数！
+
+意大利文书名为：Le cittá invisibili</p>                    
+                    <h3>任氏有无轩主人评论：</h3>
+                    <table class="table table-hover table-striped">
+    </table>
+
+                    <h3>豆瓣简介：</h3>
+                    <p>（豆瓣还没有简介）</p>
+                    <p>更多信息参见：<a href="https://book.douban.com/subject/2969648/">豆瓣链接</a>。</p>
+                    <h3>更多书籍信息</h3>
+
+
+                    <table class="table table-hover table-striped">
+
+                        <tbody>
+                            <tr>
+                                <td>出版社：</td>
+                                <td>花城出版社</td>
+                            </tr>
+                            <tr>
+                                <td>出版日期：</td>
+                                <td>1991/01/01</td>
+                            </tr>
+                            <tr>
+                                <td>印刷日期：</td>
+                                <td>1991/01/01</td>
+                            </tr>
+                            <tr>
+                                <td>版次：</td>
+                                <td>1.1</td>
+                            </tr>
+                            <tr>
+                                <td>装帧：</td>
+                                <td>普通</td>
+                            </tr>
+                            <tr>
+                                <td>千字数：</td>
+                                <td>60</td>
+                            </tr>
+                            <tr>
+                                <td>页数：</td>
+                                <td>149</td>
+                            </tr>
+                            <tr>
+                                <td>分类号：</td>
+                                <td>I.696   </td>
+                            </tr>
+                            <tr>
+                                <td>藏书位置：</td>
+                                <td>g2</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+        </div>
+    </div>  
+    <!-- The modal dialog to add more tags -->
+    <div>
+        <div class="modal fade" id="addtag">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title">增加自己的TAG</h4>
+                    </div>
+                    <div class="modal-body">
+                        <form method="post" action="/books/addtag"  id='tagform' name='tagform'>
+                            <div class="row">
+                                <div class="col-sm-4 form-group">
+                                    <label class="control-label" for="newtags">新增TAG</label>
+                                    <input type="text" class="input-xlarge" id="newtags" name="newtags" />
+                                    <p class="help-block">（用空格分隔）</p>
+                                    <input type="hidden" value="666" id="id" name="id"/>
+
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <a href="#" class="btn" data-dismiss="modal">取消</a>
+                                <button type="submit" class="btn btn-primary">保存</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Example row of columns -->
+    </div>
   </div>
 </template>
 
@@ -12,7 +174,37 @@ export default {
   name: "BookDetail",
   components: {
     TopNav,
-  }
+  },
+  data: function() {
+    return {
+      book: [],
+      isbn: '',
+    }
+  },
+  methods: {
+    getBookDetail(id) {
+      var uri='http://api.rsywx.com/book/bookByBookId/'+id;
+      fetch(uri)
+        .then(res => {
+          return res.json();
+        })
+        .then(json => {
+          var book=json.out[0];
+          book.cover="http://api.rsywx.com/book/cover/"+book.bookid+"/"+book.title+"/"+book.author+"/300";
+          this.isbn=json.out[0].isbn;
+          this.book=book;
+        });
+
+    }
+  },
+  created: function() {
+    this.book=this.getBookDetail(this.id);
+  },
+  props: [
+    'id',
+  ]
+
+  
 };
 </script>
 
